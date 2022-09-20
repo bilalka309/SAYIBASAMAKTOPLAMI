389d6fc809accaad488eb5e8c369fad2d1d8761d
bilal karahan <bilalkarahan309@gmail.com>
2022-09-20 13:56:52 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Sayı giriniz:");
+        int i=0,x= input.nextInt();
+        int y=x;
+        while(y!=0){
+            y/=10;
+            i++;
+        }
+        int total=0;
+        y=x;
+        while(y!=0){
+            int a=y%10;
+            total+=a;
+            y/=10;
+        }
+        System.out.println("TOTAL:"+total);
     }
 }
